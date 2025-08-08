4f2ef798a62cf6d945c46804de14c6b145b0c18b
Carol Vilchez <design.harls@icloud.com>
2025-08-08 10:03:22 +0200

    array de imagenes

---
+++ js/memory.js
@@ -0,0 +1,20 @@
+const grupoTarjetas = [
+  {image : './assets/img/img_1.webp'},
+  {image : './assets/img/img_2.webp'},
+  {image : './assets/img/img_3.webp'},
+  {image : './assets/img/img_4.webp'},
+  {image : './assets/img/img_5.webp'},
+  {image : './assets/img/img_6.webp'},
+  {image : './assets/img/img_7.webp'},
+  {image : './assets/img/img_8.webp'},
+  {image : './assets/img/img_1.webp'},
+  {image : './assets/img/img_2.webp'},
+  {image : './assets/img/img_3.webp'},
+  {image : './assets/img/img_4.webp'},
+  {image : './assets/img/img_5.webp'},
+  {image : './assets/img/img_6.webp'},
+  {image : './assets/img/img_7.webp'},
+  {image : './assets/img/img_8.webp'}
+
+
+];
